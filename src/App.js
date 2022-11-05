ca057511d07fc83bdba75a2c613fdac85811f08e
Emmattzon <emmattzontech@gmail.com>
2022-11-05 10:04:01 +0100

    first intial commit of project setup and config with React and Tailwind CSS

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Hero, Navbar, Footer, Main } from './components';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
